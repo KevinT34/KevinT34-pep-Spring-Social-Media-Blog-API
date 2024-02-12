76bd597d6fbecb8dd23aba43427c1731d00d0096
Kevin Toyloy <kevintoyloy34@gmail.com>
2024-02-12 23:01:52 +0000

    create get all messages

--- src/main/java/com/example/controller/SocialMediaController.java
+++ src/main/java/com/example/controller/SocialMediaController.java
@@ -1,5 +1,15 @@
 package com.example.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import com.example.entity.Message;
+import com.example.service.AccountService;
+import com.example.service.MessageService;
 
 /**
  * TODO: You will need to write your own endpoints and handlers for your controller using Spring. The endpoints you will need can be
@@ -7,6 +17,25 @@ package com.example.controller;
  * where applicable as well as the @ResponseBody and @PathVariable annotations. You should
  * refer to prior mini-project labs and lecture materials for guidance on how a controller may be built.
  */
+
+ @RestController
 public class SocialMediaController {
+    AccountService accountService;
+    MessageService messageService;
+
+    @Autowired
+    public SocialMediaController(AccountService accService, MessageService msgService) {
+        this.accountService = accService;
+        this.messageService = msgService;
+    }
+
+
+    //In progress
+    @GetMapping("messages")
+    public ResponseEntity<List<Message>> getAllMessagesHandler() {
+        
+        return ResponseEntity.status(HttpStatus.OK)
+                        .body(messageService.getAllMessages());
+    }
 
 }
--- src/main/java/com/example/service/MessageService.java
+++ src/main/java/com/example/service/MessageService.java
@@ -1,4 +1,24 @@
 package com.example.service;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.entity.Message;
+import com.example.repository.MessageRepository;
+
+@Service
 public class MessageService {
+
+    private MessageRepository messageRepo;
+
+    @Autowired
+    public MessageService(MessageRepository messageRepository){
+        this.messageRepo = messageRepository;
+    }   
+
+    public List<Message> getAllMessages() {
+        return (List<Message>) messageRepo.findAll();
+    }
 }
